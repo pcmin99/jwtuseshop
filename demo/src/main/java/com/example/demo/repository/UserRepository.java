a80be2ea4aa45d41b31d21aa1ce4d92bc318f0db
parkchanmin <parkchanmin990804@naver.com>
2024-11-02 17:57:51 +0900

    test

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,7 +6,7 @@ import com.example.demo.entity.UserEntity;
 
 
 // https://www.youtube.com/watch?v=JFTpzy7gsg0&list=PLJkjrxxiBSFCcOjy0AAVGNtIa08VLk1EJ&index=5
-// 참고 자료
+// 참고 자료 test
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     
 }
