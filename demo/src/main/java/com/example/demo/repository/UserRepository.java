7a1a33574e637024ad3c4f2982411f274bac5550
parkchanmin <parkchanmin990804@naver.com>
2024-11-02 17:56:27 +0900

    2024.11.02 프로젝트 시작

---
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.UserEntity;
+
+
+// https://www.youtube.com/watch?v=JFTpzy7gsg0&list=PLJkjrxxiBSFCcOjy0AAVGNtIa08VLk1EJ&index=5
+// 참고 자료
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    
+}
