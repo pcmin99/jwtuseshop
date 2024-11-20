1bf96a1cc032dc30cb5cb64e8fd1b8467374d2cd
parkchanmin <parkchanmin990804@naver.com>
2024-11-20 18:05:17 +0900

    11.20 user front modify no conntect

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -77,8 +77,8 @@ public class SecurityConfig {
         http.httpBasic().disable();
 
         http.authorizeHttpRequests()
-        .requestMatchers("/login", "/", "/join", "/hello","/hello1","/hello2","/pages/**").permitAll() // 이 경로들은 모두 접근 허용
-        .requestMatchers("/assets/**", "/templates/**").permitAll()
+        .requestMatchers("/login", "/", "/join", "/hello","/hello1","/hello2","/admin/**","/user/**").permitAll() // 이 경로들은 모두 접근 허용
+        .requestMatchers("/admin/**", "/templates/**").permitAll()
         //.requestMatchers("/admin").hasRole("ADMIN")  // admin 경로는 ADMIN 권한만 허용
         .anyRequest().authenticated();
         
--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -36,28 +36,26 @@ public class MainController {
 
 
 
-    @GetMapping("/hello")
-    public String hello (Model model) {
-        model.addAttribute("data", "hello!");
+    @GetMapping("/hello") // 채팅 mapping
+    public String hello () {
         return "main2";
     }
 
-    @GetMapping("/hello1")
-    public String hello1 (Model model) {
-        model.addAttribute("data", "hello!");
+    @GetMapping("/hello1") // 관리자 첫 화면 mapping
+    public String hello1 () {
         return "index";
     }
 
 
-    @GetMapping("/pages/tables.html")
+    @GetMapping("/admin/tables") // 관리자 테이블 관련 mapping
     public String tabledashboard(){
-        return "pages/tables";
+        return "admin/tables";
     }
 
 
-    @GetMapping("/pages/notifications.html")
+    @GetMapping("/admin/notifications") // 관리자 알람 관련 mapping
     public String tabledashboard1(){
-        return "pages/notifications";
+        return "admin/notifications";
     }
 
 
